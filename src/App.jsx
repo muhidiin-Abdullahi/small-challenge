14469e88ffb9a08289cabb2900c662601147d733
muhidiin-Abdullahi <muhudinho124@gmail.com>
2024-01-24 10:03:40 +0300

    small challenge

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+function App(){
+   return <div  className="bg-grey-500">
+      <div className="flex justify-center sm:gap-14 gap-5 mt-9">
+      <div>
+         <p>Day</p>
+         <h1 className="text-2xl font-bold border-2 border-grey-400 w-[90px] p-1 ">24</h1>
+      </div>
+      <div>
+         <p>Month</p>
+         <h1 className="text-2xl font-bold border-2 border-grey-400 w-[90px] p-1 ">09</h1>
+      </div>
+      <div>
+         <p>Year</p>
+         <h1 className="text-2xl font-bold border-2 border-grey-400 w-[90px] p-1 ">1985</h1>
+      </div>
+      </div>
+
+      <i class="fa-solid fa-down-long bg-blue-500 w-[30px] h-[30px] p-2 rounded-full sm:ml-[800px] ml-72 mt-5"></i>
+
+
+      <div className="sm:ml-[450px] ml-5 pt-4">
+
+      <h1 className="text-4xl font-bold text-blue-500">38 <span className="text-black">Years</span> </h1>
+      <h1 className="text-4xl font-bold text-blue-500">38 <span className="text-black">Years</span> </h1>
+      <h1 className="text-4xl font-bold text-blue-500">38 <span className="text-black">Years</span> </h1>
+
+      </div>
+
+      
+ 
+   </div>
+}
+export default App
